e4c7f8660de85d3fa6116d2fdbce428c65c446d5
Nithish kumar <nithishkumar1904@gmail.com>
2024-08-22 14:35:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const menu = document.querySelector(".ham");
-console.log(menu);
-
-const sidebar = document.querySelector(".sidebar");
-
-menu.addEventListener('click', function () {
-  sidebar.classList.toggle("show-sidebar");
-});
+var menu = document.querySelector(".ham");
+var sidebar = document.querySelector(".sidebar");
+var contents = document.querySelector(".contents");
+menu.onclick=function()
+{
+    sidebar.classList.toggle("small-sidebar");
+    contents.classList.toggle("large-contents");
+}
+sidebar.onmouseover=function()
+{
+    sidebar.classList.toggle("scrollbar");
+}
